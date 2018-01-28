a886e9f02d0466fd8903084af65cbc30fc4efde0
Filipe Natanael <filipenatanael@live.com>
2018-01-28 19:47:10 -0200

    Arrays, objects and stdClass
    Arrays, objects and stdClass

--- PHP-7-Practice/Arrays.php
+++ PHP-7-Practice/Arrays.php
@@ -9,8 +9,29 @@ array_push(
     'realname' => 'Steve Rogers',
     'codename' => 'Captain America'
   ));
+  /*
   print_r($heroes[0]['realname']);
+  Working with simple JSON
+  echo json_encode($heroes);
+  */
 
+  $json = '[{
+    "realname":"Steve Rogers",
+    "codename":"Captain America"
+  }]';
+  $data = json_decode($json);
 
+  /*
+  In order to comply with E_STRICT standards prior to PHP 5.4,
+  or the normal E_WARNING error level in PHP >= 5.4,
+  assuming you are trying to create a generic object and assign the property ( realname and codename),
+  you need to declare ( $obj ) as an object of stdClass in the global namespace:
+  */
+  $obj = new \stdClass();
+  $obj->realname = "Wade Wilson";
+  $obj->codename = 'DeadPool';
+
+  $myJson = json_encode($obj);
+  echo $myJson;
 
   ?>
