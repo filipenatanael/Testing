d01f847eba023e1b5a7823e4d24038649c38879a
Filipe Natanael <filipenatanael@live.com>
2018-01-28 20:06:31 -0200

    (Constants) Sensitive and Insensitive
    (Constants) Sensitive and Insensitive

---
+++ PHP-7-Practice/Constants.php
@@ -0,0 +1,13 @@
+<?php
+define(
+  "DATABASE_CONNECTION",
+  [
+    'host' => '127.0.0.1',
+    'databse' => 'YourDatabase',
+    'username' => 'root',
+    'password' => 'my@pass'
+  ], true);
+  /* (True) sensitive and insensitive*/
+  print_r(DATABASE_CONNECTION);
+  
+  ?>
