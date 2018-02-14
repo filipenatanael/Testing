cd2b75c4b07f59e45b01134da123984fafc79297
Filipe Natanael <filipenatanael@live.com>
2018-02-14 21:37:18 -0200

    List of registries

--- IntroductionToLaravel/app/Http/Controllers/Admin/CourseController.php
+++ IntroductionToLaravel/app/Http/Controllers/Admin/CourseController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Course;
 
 class CourseController extends Controller
 {
     public function index()
     {
-      return view('admin.courses.index');
+      $registries = Course::all();
+      return view('admin.courses.index', compact('registries'));
+    }
+
+    public function add()
+    {
+      return 'ADD Method';
     }
 }
--- IntroductionToLaravel/resources/views/admin/courses/index.blade.php
+++ IntroductionToLaravel/resources/views/admin/courses/index.blade.php
@@ -5,5 +5,38 @@
 @section('content')
   <div class="container">
    <h3 class="center">List of Courses:</h3>
+
+  <div class="row">
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Image</th>
+          <th>Published</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($registries as $registry)
+          <tr>
+            <td>{{ $registry->id }}</td>
+            <td>{{ $registry->title }}</td>
+            <td>{{ $registry->description }}</td>
+            <td> <img src="{{ asset('$registry->image') }}" alt="{{ $registry->title }}" width="120"></td>
+            <td>{{ $registry->published }}</td>
+            <td>
+              <a class="btn deep-orange" href="{{ route('admin.courses.edit',$registry->id) }}">Edit</a>
+              <a class="btn red" href="{{ route('admin.courses.delete',$registry->id) }}">Delete</a>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+    <div class="row">
+      <a class="btn blue" href="{{ route('admin.courses.add') }}">Add</a>
+    </div>
   </div>
 @endsection
