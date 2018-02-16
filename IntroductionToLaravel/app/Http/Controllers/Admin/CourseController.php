d0ba889e3c0c69ded08630c84b11338fa5ca8985
Filipe Natanael <filipenatanael@live.com>
2018-02-15 23:34:46 -0200

    Create action to update course

--- IntroductionToLaravel/app/Http/Controllers/Admin/CourseController.php
+++ IntroductionToLaravel/app/Http/Controllers/Admin/CourseController.php
@@ -50,35 +50,29 @@ class CourseController extends Controller
          return view('admin.courses.edit', compact('registry'));
     }
 
+    public function update(Request $request, $id)
+    {
+     /* Another way to get the $id, it is put it on the hidden field in edit screen */
+     $data = $request->all();
+     if(isset($data['published']))
+     {
+       $data['published'] = 'yes';
+     }else{
+       $data['published'] = 'no';
+     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     if($request->hasFile('image'))
+     {
+        $image = $request->file('image');
+        $num = rand(1000,9999);
+        $dir = "img/courses";
+        $ext = $image->guessClientExtension(); /* Get image extension */
+        $ImageName = 'Image_'.$num.".".$ext;
+        $image->move($dir, $ImageName);        /* Move to directoty and rename*/
+        $data['image'] = $dir."/".$ImageName;
+     }
+     Course::find($id)->update($data);
+     return redirect()->route('admin.courses');
+    }
 
 }
