fd13a3d11cc612f8a1ef28ef75f34e41d9d2596f
Filipe Natanael <filipenatanael@live.com>
2018-02-18 20:49:06 -0300

    Seeders

---
+++ IntroductionToLaravel/database/seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * [All Seeder] Command line: php artisan db:seed
+     * [Specific Seeder] Command line: php artisan db:seed --class=UserSeeder
+     * @return void
+     */
+    public function run()
+    {
+      $data = [
+        'name'=>"Administrator",
+        'email'=>"administrator@example.com",
+        'password'=>bcrypt("My@password"),
+      ];
+
+      if(User::where('email','=',$data['email'])->count()){
+        $user = User::where('email','=',$data['email'])->first();
+        $user->update($data);
+        echo "Created!";
+      }else{
+        User::create($data);
+        echo "Was updated!";
+      }
+    }
+}
