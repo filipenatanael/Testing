41621f516f09f87f273b682fc6db58ed34d7f776
Filipe Natanael <filipenatanael@live.com>
2018-03-07 22:13:05 -0300

    Create Navigation Component
    This is my first component using Vue JS

--- Laravel-and-vue-js-learn/resources/assets/js/app.js
+++ Laravel-and-vue-js-learn/resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('Navigation', require('./components/Navigation.vue'))
 
 const app = new Vue({
     el: '#app'
